f8b26233985841568933ecad50e16b3935d9107d
Deibson Fagner <deibson.fagner@gmail.com>
2017-10-06 07:50:06 -0300

    add repository

---
+++ src/main/java/org/home/on/estado/EstadoRepository.java
@@ -0,0 +1,9 @@
+package org.home.on.residencia;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResidenciaRepository extends JpaRepository<ResidenciaEntity, Long> {
+
+
+
+}
